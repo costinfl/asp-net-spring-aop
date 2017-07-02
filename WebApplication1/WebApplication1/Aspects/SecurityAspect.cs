70cba1b781664736de43aebe49b99f3ff4c8bfbe
Costin F <costinfl@gmail.com>
2017-07-02 17:27:01 +0300

    test

---
+++ WebApplication1/WebApplication1/Aspects/SecurityAspect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Spring.Aop;
+
+namespace WebApplication1.Aspects
+{
+    
+    public class SecurityAspect : IBeforeAdvice
+    {
+
+    }
+}
